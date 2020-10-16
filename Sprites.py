9c76bab22b8feb400a0ce4c6e57da0aeafe36dc1
The1Divider <kmanprovo@gmail.com>
2020-10-15 23:00:30 -0700

    Added inventory display

--- Sprites.py
+++ Sprites.py
@@ -17,6 +17,64 @@ class Menus:
                 "5) Load/Save      \n" \
                 "6) Exit             "
 
+    class InventoryMenus:
+        @staticmethod
+        def inventoryMenuSpacing(menu_list, special_list):
+            max_length = len(max(menu_list, key=len))
+            for i in menu_list:
+                ind = menu_list.index(i)
+                if i in special_list:
+                    while len(i) < max_length:
+                        i = i[:-2] + "-" + i[-2:]
+                        menu_list[ind] = i
+                while len(i) < max_length:
+                    i = i[:-3] + " " + i[-3:]
+                    menu_list[ind] = i
+            return "".join(menu_list)
+
+        @staticmethod
+        def inventoryArmourMenu(helmet, chest, leg, boots, ring1, ring2):
+            menu_line1 = " ------------------------------- \n"
+            menu_line2 = "| Armour |\n"
+            menu_line3 = "|-------------------------------|\n"
+            menu_line4 = "| Helmet   | {} |\n".format(helmet)
+            menu_line5 = "| Chest    | {} |\n".format(chest)
+            menu_line6 = "| Leggings | {} |\n".format(leg)
+            menu_line7 = "| Boots    | {} |\n".format(boots)
+            menu_line8 = "| Ring 1   | {} |\n".format(ring1)
+            menu_line9 = "| Ring 2   | {} |\n".format(ring2)
+            menu_linex = " ------------------------------- \n"
+            menu_list = [menu_line1, menu_line2, menu_line3, menu_line4, menu_line5,
+                         menu_line6, menu_line7, menu_line8, menu_line9, menu_linex]
+            return Menus.InventoryMenus.inventoryMenuSpacing(menu_list, [menu_line1, menu_line3, menu_linex])
+
+        @staticmethod
+        def inventoryWeaponMenu(weapon1, weapon2, quiver):
+            menu_line1 = " ------------------------------- \n"
+            menu_line2 = "| Weapons |\n"
+            menu_line3 = "|-------------------------------|\n"
+            menu_line4 = " Weapon 1 | {} |\n".format(weapon1)
+            menu_line5 = " Weapon 2 | {} |\n".format(weapon2)
+            menu_line6 = " Quiver   | {} |\n".format(quiver)
+            menu_line7 = " ------------------------------- \n"
+            menu_list = [menu_line1, menu_line2, menu_line3, menu_line4, menu_line5, menu_line6, menu_line7]
+            return Menus.InventoryMenus.inventoryMenuSpacing(menu_list, [menu_line1, menu_line3, menu_line7])
+
+        @staticmethod
+        def inventoryBagMenu(item1, item2, item3, item4, item5, item6, item7, item8, item9, itemx):
+            menu_line1 = " ------------------------------- \n"
+            menu_line2 = "| Bag |\n"
+            menu_line3 = "|-------------------------------|\n"
+            menu_line4 = "| 1 - {} | 2 - {} |\n".format(item1, item2)
+            menu_line5 = "| 3 - {} | 4 - {} |\n".format(item3, item4)
+            menu_line6 = "| 5 - {} | 6 - {} |\n".format(item5, item6)
+            menu_line7 = "| 7 - {} | 8 - {} |\n".format(item7, item8)
+            menu_line8 = "| 9 - {} | 10 - {} |\n".format(item9, itemx)
+            menu_line9 = " ------------------------------- \n"
+            menu_list = [menu_line1, menu_line2, menu_line3, menu_line4, menu_line5,
+                         menu_line6, menu_line7, menu_line8, menu_line9]
+            return Menus.InventoryMenus.inventoryMenuSpacing(menu_list, [menu_line1, menu_line3, menu_line9])
+
     shop_menu_main = None
     shop_menu_buy = None
     shop_menu_sell = None
@@ -24,6 +82,14 @@ class Menus:
 
 
 class Landscapes:
+    main_village = "    888              \n" \
+                   "    8888               ____[]\n" \
+                   "    888               /    []\\\n" \
+                   "   _| |              /     [] \\\n" \
+                   " /  | |    ____     /          \\\n" \
+                   "/______\\  /    \\     |   __   |\n" \
+                   "|  _   |  |    |     |  |  |  | \n" \
+                   "| | |  |  | [] |     |  |  |  |  \n"
     mountain = "       /\\         \n" \
                "      /AA\\        \n" \
                "     /-\\/-\\      \n" \
@@ -85,3 +151,6 @@ class Enemies:
                      "    `+'(  (   \\    ) \n" \
                      "       //  \\   |_./  \n" \
                      "     '~' '~----'      \n"
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import random
+import time
 from Sprites import *
 from InventorySystem import *
 global inventory
+
 
 
 def startMenu():
@@ -28,11 +31,10 @@ def mainMenu():
     while True:
         selection = input(">").lower()
         if selection in ["1", "quest"]:
-            break
+            startGame()
 
         elif selection in ["2", "inventory"]:
-            break
-
+            inventoryDisplay()
         elif selection in ["3", "shop"]:
             break
 
@@ -56,6 +58,109 @@ def mainMenu():
 
         else:
             print("Invalid selection")
+
+
+def inventoryDisplay():
+    armour_list, weapon_list, bag_list = Inventory.inventorySetup(inventory)
+    armour = Menus.InventoryMenus.inventoryArmourMenu(armour_list[0][1].name, armour_list[1][1].name,
+                                                      armour_list[2][1].name, armour_list[3][1].name,
+                                                      armour_list[4][1].name, armour_list[5][1].name)
+    weapon = Menus.InventoryMenus.inventoryWeaponMenu(weapon_list[0][1].name, weapon_list[1][1].name,
+                                                      weapon_list[2][1].name)
+    bag = Menus.InventoryMenus.inventoryBagMenu(bag_list[0], bag_list[1], bag_list[2], bag_list[3], bag_list[4],
+                                                bag_list[5], bag_list[6], bag_list[7], bag_list[8], bag_list[9])
+
+    def armour_menu():
+        print(armour)
+        selection = None
+        while selection not in [1, 2, 3]:
+            try:
+                selection = int(input("1) Select Item, 2) Next Page, 3) Exit\n"))
+                if selection not in [1, 2, 3]:
+                    print("Invalid Selection")
+                    selection = input("1) Select Item, 2) Next Page, 3) Exit\n")
+                else:
+                    break
+            except ValueError:
+                print("Invalid Selection")
+                selection = input("1) Select Item, 2) Next Page, 3) Exit\n")
+
+        if selection == 1:
+            pass
+        elif selection == 2:
+            return weapon_menu()
+        elif selection == 3:
+            return mainMenu()
+
+    def weapon_menu():
+        print(weapon)
+        selection = None
+        while True:
+            try:
+                selection = int(input("1) Select Item, 2) Next Page 3) Previous Page, 4) Exit\n"))
+                if selection not in [1, 2, 3, 4]:
+                    print("Invalid Selection")
+                    selection = input("1) Select Item, 2) Next Page, 3) Previous Page, 4) Exit\n")
+                else:
+                    break
+            except ValueError:
+                print("Invalid Selection")
+                selection = input("1) Select Item, 2) Next Page, 3) Previous Page, 4) Exit\n")
+
+        if selection == 1:
+            pass
+        elif selection == 2:
+            return bag_menu()
+        elif selection == 3:
+            return armour_menu()
+        elif selection == 4:
+            return mainMenu()
+
+    def bag_menu():
+        print(bag)
+        selection = None
+        while selection not in [1, 2, 3]:
+            try:
+                selection = int(input("1) Select Item, 2) Previous Page, 3) Exit\n"))
+                if selection not in [1, 2, 3]:
+                    print("Invalid Selection")
+                    selection = input("1) Select Item, 2) Previous Page, 3) Exit\n")
+                else:
+                    break
+            except ValueError:
+                print("Invalid Selection")
+                selection = input("1) Select Item, 2) Previous Page, 3) Exit\n")
+
+        if selection == 1:
+            pass
+        elif selection == 2:
+            return weapon_menu()
+        elif selection == 3:
+            return mainMenu()
+
+    armour_menu()
+
+
+
+def startGame():
+    print(Landscapes.main_village)
+    print("You start your adventure in the village")
+    start_choice = input("Which direction would you like to go?\n").lower()
+    while start_choice not in ["north", "south", "east", "west", "nowhere"]:
+        start_choice = input("Invalid direction (north, south, east, west)\n")
+
+    if start_choice == "nowhere":
+        lazy_selection = random.randint(1,4)
+        if lazy_selection == 1:
+            print("You decide to take a nap")
+        elif lazy_selection == 2:
+            print("You sit down and start playing with the grass")
+        elif lazy_selection == 3:
+            print("Someone hits you in the head with a rock")
+        elif lazy_selection == 4:
+            print("Your neighbor reports you for soliciting")
+        time.sleep(.5)
+        mainMenu()
 
 
 if __name__ == "__main__":
