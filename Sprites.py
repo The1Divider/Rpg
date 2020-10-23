9638f5e1bbefb0b895f4d8aee0a69db7812f12fc
The1Divider <kmanprovo@gmail.com>
2020-10-23 07:11:26 -0700

    Formatting changes

--- Sprites.py
+++ Sprites.py
@@ -20,6 +20,7 @@ class Menus:
     class InventoryMenus:
         @staticmethod
         def inventory_menu_spacing(menu_list, special_list):
+            """Method to align inventory display regardless of item name size"""
             max_length = len(max(menu_list, key=len))
             for i in menu_list:
                 ind = menu_list.index(i)
@@ -33,35 +34,40 @@ class Menus:
             return "".join(menu_list)
 
         @staticmethod
-        def inventory_armour_menu(helmet, chest, leg, boots, ring1, ring2):
+        def inventory_armour_menu(armour_list: list):
+            for i in armour_list:
+                helmet, chest, leg, boots, ring1, ring2 = armour_list
             menu_line1 = " ------------------------------- \n"
             menu_line2 = "| Armour |\n"
             menu_line3 = "|-------------------------------|\n"
-            menu_line4 = "| Helmet   | {} |\n".format(helmet)
-            menu_line5 = "| Chest    | {} |\n".format(chest)
-            menu_line6 = "| Leggings | {} |\n".format(leg)
-            menu_line7 = "| Boots    | {} |\n".format(boots)
-            menu_line8 = "| Ring 1   | {} |\n".format(ring1)
-            menu_line9 = "| Ring 2   | {} |\n".format(ring2)
-            menu_linex = " ------------------------------- \n"
+            menu_line4 = "| Helmet   | {} |\n".format(helmet.name)
+            menu_line5 = "| Chest    | {} |\n".format(chest.name)
+            menu_line6 = "| Leggings | {} |\n".format(leg.name)
+            menu_line7 = "| Boots    | {} |\n".format(boots.name)
+            menu_line8 = "| Ring 1   | {} |\n".format(ring1.name)
+            menu_line9 = "| Ring 2   | {} |\n".format(ring2.name)
+            menu_line10 = " ------------------------------- \n"
             menu_list = [menu_line1, menu_line2, menu_line3, menu_line4, menu_line5,
-                         menu_line6, menu_line7, menu_line8, menu_line9, menu_linex]
-            return Menus.InventoryMenus.inventory_menu_spacing(menu_list, [menu_line1, menu_line3, menu_linex])
+                         menu_line6, menu_line7, menu_line8, menu_line9, menu_line10]
+            return Menus.InventoryMenus.inventory_menu_spacing(menu_list, [menu_line1, menu_line3, menu_line10])
 
         @staticmethod
-        def inventory_weapon_menu(weapon1, weapon2, quiver):
+        def inventory_weapon_menu(weapon_list: list):
+            weapon1, weapon2, quiver = weapon_list
             menu_line1 = " ------------------------------- \n"
             menu_line2 = "| Weapons |\n"
             menu_line3 = "|-------------------------------|\n"
-            menu_line4 = "| Weapon 1 | {} |\n".format(weapon1)
-            menu_line5 = "| Weapon 2 | {} |\n".format(weapon2)
+            menu_line4 = "| Weapon 1 | {} |\n".format(weapon1.name)
+            menu_line5 = "| Weapon 2 | {} |\n".format(weapon2.name)
             menu_line6 = "| Quiver   | {} |\n".format(quiver)
             menu_line7 = " ------------------------------- \n"
-            menu_list = [menu_line1, menu_line2, menu_line3, menu_line4, menu_line5, menu_line6, menu_line7]
+            menu_list = [menu_line1, menu_line2, menu_line3, menu_line4,
+                         menu_line5, menu_line6, menu_line7]
             return Menus.InventoryMenus.inventory_menu_spacing(menu_list, [menu_line1, menu_line3, menu_line7])
 
         @staticmethod
-        def inventory_bag_menu(item1, item2, item3, item4, item5, item6, item7, item8, item9, itemx):
+        def inventory_bag_menu(item_list: list):
+            item1, item2, item3, item4, item5, item6, item7, item8, item9, item10 = item_list
             menu_line1 = " ------------------------------- \n"
             menu_line2 = "| Bag |\n"
             menu_line3 = "|-------------------------------|\n"
@@ -69,7 +75,7 @@ class Menus:
             menu_line5 = "| 3 - {} | 4 - {} |\n".format(item3, item4)
             menu_line6 = "| 5 - {} | 6 - {} |\n".format(item5, item6)
             menu_line7 = "| 7 - {} | 8 - {} |\n".format(item7, item8)
-            menu_line8 = "| 9 - {} | 10 - {} |\n".format(item9, itemx)
+            menu_line8 = "| 9 - {} | 10 - {} |\n".format(item9, item10)
             menu_line9 = " ------------------------------- \n"
             menu_list = [menu_line1, menu_line2, menu_line3, menu_line4, menu_line5,
                          menu_line6, menu_line7, menu_line8, menu_line9]
@@ -90,6 +96,7 @@ class Landscapes:
                    "/______\\  /    \\     |   __   |\n" \
                    "|  _   |  |    |     |  |  |  | \n" \
                    "| | |  |  | [] |     |  |  |  |  \n"
+
     mountain = "       /\\         \n" \
                "      /AA\\        \n" \
                "     /-\\/-\\      \n" \
@@ -98,6 +105,15 @@ class Landscapes:
                "  /    __    \\    \n" \
                " /    {  }    \\   \n" \
                "/____/    \\____\\ \n"
+
+    mountain_range = "           /\\              \n" \
+                     "          /  \\             \n" \
+                     "   /\\    /    \\  /\\      \n" \
+                     "  /  \\  /      \\/  \\     \n" \
+                     " / /\\ \\/       /    \\   \n" \
+                     "/ /  \\ \\      /      \\  \n" \
+                     " /    \\ \\    /        \\ \n" \
+                     "/      \\ \\  /          \\\n"
 
     blocked_mountain = "       /\\         \n" \
                        "      /AA\\        \n" \
@@ -151,3 +167,4 @@ class Enemies:
                      "    `+'(  (   \\    ) \n" \
                      "       //  \\   |_./  \n" \
                      "     '~' '~----'      \n"
+print(Landscapes.mountain_range)
